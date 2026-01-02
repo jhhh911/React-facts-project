1edf049f32a1fed9c398321d5278849cc85156c2
James Heiniger <james.heiniger911@gmail.com>
2026-01-02 14:54:06 +1030

    Added main content

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+<main>
+  <img src={'../src/assets/react.svg'} width={40}/>
+  <h1>Fun facts about React</h1>
+  <ul>
+    <li>Was first released in 2012</li>
+    <li>Was originally created by Jordan Walke</li>
+    <li>Has well over 100k stars on GitHub</li>
+    <li>Is maintained by Meta</li>
+    <li>Powers thousands of enterprise apps, including mobile apps</li>
+  </ul>
+</main>)
